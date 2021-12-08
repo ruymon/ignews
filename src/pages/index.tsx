7b548c8bd4ad78362c169091a65480a9ff3dc450
Ruy Monteiro <ruystfrancis@gmail.com>
2021-12-08 15:47:14 -0300

    Home page layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,28 @@
-// import styles from '../styles/home.module.scss';
 import Head from 'next/head';
+
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>
-          Início | ig.news
+          Home | ig.news
         </title>
       </Head>
 
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>fot $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   )
 }
