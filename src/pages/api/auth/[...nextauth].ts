90c6dcce0365b4cec341150c9e5bd61bdb2b4f8e
Ruy Monteiro <ruystfrancis@gmail.com>
2021-12-08 21:52:11 -0300

    Fauna DB Integration

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { query as q } from 'faunadb';
+
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -13,4 +17,21 @@ export default NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async signIn({user, account, profile}) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.Create(q.Collection('users'), { 
+            data: { email },
+          })
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 })
