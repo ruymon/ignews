4580d04f22326c8a61fc732c0ba37b68ece3cbdb
Ruy Monteiro <ruystfrancis@gmail.com>
2021-12-16 10:08:26 -0300

    validate if user exists in database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -21,12 +21,29 @@ export default NextAuth({
     async signIn({user, account, profile}) {
       const { email } = user;
 
-      try {
+      try{
         await fauna.query(
-          q.Create(q.Collection('users'), { 
-            data: { email },
-          })
-        );
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
 
         return true;
       } catch {
